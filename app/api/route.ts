72cc06e5f614f1a4f559d615775c2530b5714ef4
Tumi Le (Lê Hoàng Dũng) <lehoangdung@live.com>
2024-02-27 22:09:19 +0700

    Update .gitignore

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,3 +3,5 @@ import {NextRequest, NextResponse} from "next/server";
 export async function GET(request: NextRequest) {
   return NextResponse.json({time: new Date().toLocaleString()});
 }
+
+// cnnstr: mongodb+srv://tumivn:<password>@cluster0.l5fisqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
